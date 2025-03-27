3a62d8274b5e67fec3822d2837b4eb270401667f
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-03-27 03:50:07 +0000

    Add BrowserRouter to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import { ThemeProvider } from './components/theme-provider'
@@ -8,9 +9,11 @@ import { Toaster } from './components/ui/toast'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
-      <Toaster />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App />
+        <Toaster />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
