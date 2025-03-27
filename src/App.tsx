65898596c79e22ce2f7c03b1f8fb79d7aa60bcd2
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-03-27 02:46:11 +0000

    Update App with Theme Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { ThemeProvider } from './components/theme-provider'
-import { Navigation } from './components/navigation'
-import { Footer } from './components/footer'
-import { Home } from './pages/home'
-import { Post } from './pages/post'
-import { Admin } from './pages/admin'
+import { ThemeProvider } from "./components/theme-provider"
+import { Navigation } from "./components/navigation"
 
-export default function App() {
+export function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="blog-theme">
-      <Router>
-        <div className="min-h-screen flex flex-col">
-          <Navigation />
-          <main className="flex-1">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/post/:id" element={<Post />} />
-              <Route path="/admin" element={<Admin />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+    <ThemeProvider defaultTheme="system" storageKey="app-theme">
+      <div className="min-h-screen bg-background text-foreground">
+        <Navigation />
+        {/* Your other app content */}
+      </div>
     </ThemeProvider>
   )
 }
